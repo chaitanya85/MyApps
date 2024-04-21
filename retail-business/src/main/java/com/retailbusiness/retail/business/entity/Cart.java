e0e92d51397b96a312ed14025a42c5051c3b4f0a
chaitanya <chaitanyak854@gmail.com>
2024-04-21 08:54:25 +0530

    updated cart class with member variables

--- retail-business/src/main/java/com/retailbusiness/retail/business/entity/Cart.java
+++ retail-business/src/main/java/com/retailbusiness/retail/business/entity/Cart.java
@@ -2,4 +2,7 @@ package com.retailbusiness.retail.business.entity;
 
 public class Cart {
   private int userId;
+  private String userName;
+  private float cartTotal;
+  private String itemNames;
 }
